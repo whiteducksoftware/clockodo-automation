c450e4f1e73401bef2581588357c44475a1a7b26
Suad Wolgram <Suad@Wolgram.com>
2020-12-18 16:23:39 +0100

    feat: Time trigger function

---
+++ src/function/ClockodoTimeTrigger.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+
+namespace Company.Function
+{
+    public class ClockodoTimeTrigger
+    {
+        private static HttpClient httpClient = new HttpClient();
+
+        [FunctionName("ClockodoTimeTrigger")]
+        public async Task Run([TimerTrigger("5 */0 * * * *")] TimerInfo myTimer, ILogger log)
+        {
+            var azureServiceTokenProvider = new AzureServiceTokenProvider();
+            var kv = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback));
+            string secretName = (await kv.GetSecretAsync("https://clock-dev-akv.vault.azure.net/", "secretName")).Value;
+            string secretKey = (await kv.GetSecretAsync("https://clock-dev-akv.vault.azure.net/", "secretKey")).Value;
+
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic",
+            Convert.ToBase64String(Encoding.UTF8.GetBytes($"{secretName}:{secretKey}")));
+
+            var httpResponse = await httpClient.GetAsync("https://my.clockodo.com/api/clock");
+
+            log.LogInformation($"Status Code: {httpResponse.StatusCode} Data: {await httpResponse.Content.ReadAsStringAsync()}");
+        }
+    }
+}
