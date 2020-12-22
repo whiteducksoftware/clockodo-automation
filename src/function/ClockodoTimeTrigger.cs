4b618fc0230a3fb47695022436bee52fe6df1ab8
Suad Wolgram <Suad@Wolgram.com>
2020-12-22 16:27:27 +0100

    chor: Update naming

--- src/function/ClockodoTimeTrigger.cs
+++ src/function/ClockodoTimeTrigger.cs
@@ -17,13 +17,12 @@ namespace Company.Function
         [FunctionName("ClockodoTimeTrigger")]
         public async Task Run([TimerTrigger("1 */0 * * * *")] TimerInfo myTimer, ILogger log)
         {
-
             var keyvaultName = GetEnvironmentVariable("KEYVAULT_NAME");
 
             var azureServiceTokenProvider = new AzureServiceTokenProvider();
             var kv = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback));
-            string secretName = (await kv.GetSecretAsync($"{keyvaultName}", "secretName")).Value;
-            string secretKey = (await kv.GetSecretAsync($"{keyvaultName}", "secretKey")).Value;
+            string secretName = (await kv.GetSecretAsync($"https://{keyvaultName}.vault.azure.net/", "secretName")).Value;
+            string secretKey = (await kv.GetSecretAsync($"https://{keyvaultName}.vault.azure.net/", "secretKey")).Value;
 
             httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic",
             Convert.ToBase64String(Encoding.UTF8.GetBytes($"{secretName}:{secretKey}")));
