47f6c629083f7fcd7c50f967d1fe74590d6e4ef1
Suad Wolgram <Suad@Wolgram.com>
2021-01-07 21:05:51 +0100

    chor: Added new Model for testing

--- src/function/TimeTriggerClock/ClockodoTimeTrigger.cs
+++ src/function/TimeTriggerClock/ClockodoTimeTrigger.cs
@@ -2,13 +2,15 @@ using System;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Microsoft.Azure.KeyVault;
 using Microsoft.Azure.Services.AppAuthentication;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
+using TimeTriggerClock.models;
 
-namespace Company.Function
+namespace TimeTriggerClock.function
 {
     public class ClockodoTimeTrigger
     {
@@ -29,7 +31,12 @@ namespace Company.Function
 
             var httpResponse = await httpClient.GetAsync("https://my.clockodo.com/api/clock");
 
+            var content = await httpResponse.Content.ReadAsStringAsync();
+
+            var tasks = JsonSerializer.Deserialize<EntrieModel.Rootobject>(content);
+
             log.LogInformation($"Status Code: {httpResponse.StatusCode} Data: {await httpResponse.Content.ReadAsStringAsync()}");
+
         }
 
         public static string GetEnvironmentVariable(string keyvaultName)
