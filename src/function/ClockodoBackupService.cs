486a4faeef18c14fce80e564c6e1178b1f03a80c
Martin Brandl <mjibrandl@gmail.com>
2021-02-10 11:51:55 +0100

    feat: refactoring

---
+++ src/function/ClockodoBackupService.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Azure.Storage.Blobs;
+using CsvHelper;
+using function.models;
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
+
+namespace function
+{
+    static class ClockodoBackupService
+    {
+        public static async Task BackupAsync(string keyvaultName, HttpClient httpClient, string connectionString, DateTime dateTimeSince, DateTime dateTimeUntil, string backupFileName)
+        {
+            var azureServiceTokenProvider = new AzureServiceTokenProvider();
+            var kv = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback));
+            var clockodoApiUser = (await kv.GetSecretAsync($"https://{keyvaultName}.vault.azure.net/", "ClockodoApiUser")).Value;
+            var clockodoApiKey = (await kv.GetSecretAsync($"https://{keyvaultName}.vault.azure.net/", "ClockodoApiKey")).Value;
+
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic",
+                Convert.ToBase64String(Encoding.UTF8.GetBytes($"{clockodoApiUser}:{clockodoApiKey}")));
+
+            var requestUri = QueryHelpers.AddQueryString("https://my.clockodo.com/api/entries",
+                new Dictionary<string, string>
+                {
+                    { "time_since", $"{dateTimeSince:yyyy'-'MM'-'dd HH:mm: ss}"},
+                    { "time_until", $"{dateTimeUntil:yyyy'-'MM'-'dd HH:mm: ss}" }
+                });
+
+            var httpResponse = await httpClient.GetAsync(requestUri);
+            var content = await httpResponse.Content.ReadAsStringAsync();
+
+            var entryModel = JsonSerializer.Deserialize<EntryModel.Rootobject>(content);
+
+            var client = new BlobContainerClient(connectionString, "backups");
+
+            using (Stream stream = new MemoryStream(Encoding.UTF8.GetBytes(JsonToCsv(entryModel.entries, ","))))
+            {
+                
+                if (!client.GetBlobClient(backupFileName).Exists())
+                {
+                    //await client.UploadBlobAsync($"{localDate.ToString($"MM-dd-yyyy-backup")}.csv", stream);
+                    await client.UploadBlobAsync($"{backupFileName}", stream);
+                }
+                else
+                {
+                    Console.WriteLine("Backup exists");
+                }
+            }
+        }
+
+        public static string JsonToCsv(EntryModel.Entry[] tasks, string delimiter)
+        {
+            using (var writer = new StringWriter())
+            {
+                using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+                {
+                    csv.Configuration.Delimiter = delimiter;
+                    csv.WriteHeader<EntryModel.Entry>();
+                    csv.NextRecord();
+                    csv.WriteRecords(tasks);
+                }
+
+                return writer.ToString();
+            }
+        }
+    }
+}
--- src/function/TimeTriggerClockodo.cs
+++ src/function/TimeTriggerClockodo.cs
@@ -1,91 +1,30 @@
 using System;
 using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Text;
-using System.Text.Json;
 using System.Threading.Tasks;
-using Microsoft.Azure.KeyVault;
-using Microsoft.Azure.Services.AppAuthentication;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
-using function.models;
-using System.IO;
-using System.Globalization;
-using CsvHelper;
-using Azure.Storage.Blobs;
 
 namespace function
 {
     public class TimeTriggerClockodo
     {
-        private static HttpClient httpClient = new HttpClient();
+        private static readonly HttpClient HttpClient = new HttpClient();
 
         [FunctionName("TimeTriggerClockodo")]
         public async Task Run([TimerTrigger("1 */0 * * * *")] TimerInfo myTimer, ILogger log)
         {
             var keyvaultName = GetEnvironmentVariable("KEYVAULT_NAME");
-            var containerName = GetEnvironmentVariable("CONTAINER_NAME");
             var connectionString = GetEnvironmentVariable("AzureWebJobsStorage");
 
-            var localDate = DateTime.Now;
-
-            var azureServiceTokenProvider = new AzureServiceTokenProvider();
-            var kv = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback));
-            var ClockodoApiUser = (await kv.GetSecretAsync($"https://{keyvaultName}.vault.azure.net/", "ClockodoApiUser")).Value;
-            var ClockodoApiKey = (await kv.GetSecretAsync($"https://{keyvaultName}.vault.azure.net/", "ClockodoApiKey")).Value;
-
-            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic",
-            Convert.ToBase64String(Encoding.UTF8.GetBytes($"{ClockodoApiUser}:{ClockodoApiKey}")));
-
-            var subDate = DateTime.Now.AddDays(-1).AddMonths(-1);
-
-            var httpResponse = await httpClient.GetAsync($"https://my.clockodo.com/api/entries?time_since={subDate.ToString("yyyy'-'MM'-'dd HH:mm:ss")}&time_until={localDate.ToString("yyyy'-'MM'-'dd HH:mm:ss")}");
-            var content = await httpResponse.Content.ReadAsStringAsync();
-
-            var entryModel = JsonSerializer.Deserialize<EntryModel.Rootobject>(content);
-
-            var client = new BlobContainerClient(connectionString, "backups");
-
-            using (Stream stream = new MemoryStream(Encoding.UTF8.GetBytes(JsonToCsv(entryModel.entries, ","))))
-            {
-                var rand = new Random();
-                var name = ($"Daylybackup/{localDate.ToString($"MM-dd-yyyy-backup")}.csv");
-                if (!client.GetBlobClient(name).Exists())
-                {
-                    //await client.UploadBlobAsync($"{localDate.ToString($"MM-dd-yyyy-backup")}.csv", stream);
-                    await client.UploadBlobAsync($"{name}", stream);
-                }
-                else
-                {
-                    Console.WriteLine("Backup exists");
-                }
-            }
+            var now = DateTime.Now;
+            await ClockodoBackupService.BackupAsync(keyvaultName, HttpClient, connectionString,
+                now.AddDays(-1).AddMonths(-1), now, $"Daylybackup/{now.ToString($"MM-dd-yyyy-backup")}.csv");
         }
 
         public static string GetEnvironmentVariable(string environmentVariable)
         {
-            return System.Environment.GetEnvironmentVariable(environmentVariable, EnvironmentVariableTarget.Process);
+            return Environment.GetEnvironmentVariable(environmentVariable, EnvironmentVariableTarget.Process);
         }
-
-        public static string JsonToCsv(EntryModel.Entry[] tasks, string delimiter)
-        {
-            using (var writer = new StringWriter())
-            {
-                using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
-                {
-                    csv.Configuration.Delimiter = delimiter;
-
-                    csv.WriteHeader<EntryModel.Entry>();
-
-                    csv.NextRecord();
-
-                    csv.WriteRecords<EntryModel.Entry>(tasks);
-                }
-
-                return writer.ToString();
-            }
-        }
-
     }
 }
 
